b65ab59c5fc1caa4524012e3b3d872e930226510
unknown <diogosilvabianchini@gmail.com>
2024-12-29 11:31:27 -0300

    Correção no arquivo de mongoConfig.

--- src/config/mongoConfig.js
+++ src/config/mongoConfig.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose")
 
 const mongoConnection = () => {
-    const {DB_MONGO_NAME, DB_MONGO_PASSWORD, DB_MONGO_PORT, DB_MONGO_USER_NAME, NODE_ENV, DB_HOST} = process.env
+    const {DB_MONGO_PASSWORD, DB_MONGO_USER_NAME, NODE_ENV, DB_HOST} = process.env
 
-    const host = NODE_ENV == "development" ? "0.0.0.0" : DB_HOST
-
-    const urlConnect = NODE_ENV == "development" ?
-    `mongodb://${DB_MONGO_USER_NAME}:${DB_MONGO_PASSWORD}@${host}:${DB_MONGO_PORT}/${DB_MONGO_NAME}?authSource=admin`:
-    `mongodb+srv://${DB_MONGO_USER_NAME}:${DB_MONGO_PASSWORD}@cluster0.j2ukr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    const urlConnect = `mongodb+srv://${DB_MONGO_USER_NAME}:${DB_MONGO_PASSWORD}@cluster0.j2ukr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    
     
     const mongoConfig = {
         serverSelectionTimeoutMS: 20000, 
